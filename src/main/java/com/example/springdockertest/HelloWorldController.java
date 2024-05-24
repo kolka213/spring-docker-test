22aa4239b879f724781e32665f23b4b3e67ad18a
Pascal Schwabe <pascal.schwabe@iq-image.com>
2024-05-24 14:57:56 +0200

    edited hello world message

--- src/main/java/com/example/springdockertest/HelloWorldController.java
+++ src/main/java/com/example/springdockertest/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello from Watchtower!";
+        return "Hello World again!";
     }
 }
